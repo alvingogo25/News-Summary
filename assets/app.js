a8b0bbe1cdf09a1b5ad8e525b84bfb75e54282fc
jbusta93 <jbusta93@gmail.com>
2018-01-22 18:27:55 -0800

    Updated with chartjs implementation

--- assets/app.js
+++ assets/app.js
@@ -2,17 +2,17 @@
 $(document).ready(function() {
   var queryURL = "https://newsapi.org/v2/top-headlines?language=en&apiKey=75784847d8a1404aa2a52bacd32952c3"
   $.ajax({
-    url: queryURL,
-    method: "GET",
-    cache: false
-  })
-  .done(function(response) {
-    console.log(response);
-    let urls = response.articles
-    for (var i = 0; i < urls.length; i++){
-      makeList(urls[i]);
-    };
-  });
+      url: queryURL,
+      method: "GET",
+      cache: false
+    })
+    .done(function(response) {
+      console.log(response);
+      let urls = response.articles
+      for (var i = 0; i < urls.length; i++) {
+        makeList(urls[i]);
+      };
+    });
 })
 
 // lists article and descriptions
@@ -37,6 +37,7 @@ var p = $('<p id="this-summary">');
 
 // summarizes selected article
 $('body').on('click', '#short', function() {
+  summaryText = "";
   $('#this-summary').empty();
   $('#sentiment-chart').empty();
   var url = $(this).attr('data-url');
@@ -53,7 +54,7 @@ $('body').on('click', '#short', function() {
     //   "cache-control": "no-cache"
     // }
   }
-  $.ajax(summarize).done(function (response) {
+  $.ajax(summarize).done(function(response) {
     console.log('summary', response);
     var itemArray = response.items
     for (var i = 0; i < itemArray.length; i++) {
@@ -63,40 +64,79 @@ $('body').on('click', '#short', function() {
 
     $('#summary-here').html(p);
     $('#summary-here').append('<button id="sentiment" class="btn btn-dark">Sentiment</button>')
-  }).fail(function(){
+  }).fail(function() {
     $('#summary-here').text('Host URL failed to load.')
   });
 
   $('#summ').modal('show')
 });
 
+
+//Generate Sentiment Chart
 $('body').on('click', '#sentiment', function() {
+  $("#sentiment-chart").empty();
   var sentimentPacket = {
     "documents": [{
       "id": "1",
       "language": "en",
       "text": summaryText
-    }
-  ]
+    }]
+  }
+  $.ajax({
+      url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": "7558bc9d25074ba2bf29fee41f070cd1"
+      },
+      method: "POST",
+      // Request body
+      data: JSON.stringify(sentimentPacket)
+    })
+    .done(function(response) {
+      console.log(response);
+      function precisionRound(number, precision) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
 }
-$.ajax({
-  url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
-  headers: {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Ocp-Apim-Subscription-Key": "7558bc9d25074ba2bf29fee41f070cd1"
+
+var sentimentPercent = response.documents[0].score * 100;
+var sentimentScoreRounded = precisionRound(sentimentPercent,1);
+      // $('#sentiment-chart').append(response.documents[0].score)
+        $('#sentiment-chart').append("<canvas id=\"myChart\" width=\"400\" height=\"400\"></canvas>");
+        var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'horizontalBar',
+  data: {
+    labels: [sentimentScoreRounded + "% Positive"],
+    datasets: [{
+      label: "Sentiment Score: " + sentimentScoreRounded,
+      data: [sentimentScoreRounded],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)'
+      ],
+      borderWidth: 1
+      // barThickness: 5
+    }]
   },
-  method: "POST",
-  // Request body
-  data: JSON.stringify(sentimentPacket)
-})
-.done(function(response) {
-  console.log(response);
-  $('#sentiment-chart').append(response.documents[0].score)
-})
-.fail(function() {
-  alert("error");
+  options: {
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
 });
+
+    })
+    .fail(function() {
+      alert("error");
+    });
 });
 
 
@@ -108,13 +148,13 @@ $('#run-search').click(function() {
   var newqueryURL = "https://newsapi.org/v2/everything?q=" + query + "&language=en&apiKey=75784847d8a1404aa2a52bacd32952c3"
   console.log(query);
   $.ajax({
-    url: newqueryURL,
-    method: "GET"
-  })
-  .done(function(responze) {
-    let urlz = responze.articles
-    for (var j = 0; j < urlz.length; j++){
-      makeList(urlz[j]);
-    };
-  });
+      url: newqueryURL,
+      method: "GET"
+    })
+    .done(function(responze) {
+      let urlz = responze.articles
+      for (var j = 0; j < urlz.length; j++) {
+        makeList(urlz[j]);
+      };
+    });
 })
